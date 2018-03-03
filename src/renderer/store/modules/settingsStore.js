ff756346ce2e056837c9fd8299c9d6a3621a2d7b
mtobeiyf <mtobeiyf@163.com>
2018-03-04 01:51:40 +0800

    Add katex support

--- src/renderer/store/modules/settingsStore.js
+++ src/renderer/store/modules/settingsStore.js
@@ -1,5 +1,20 @@
 import db from '../../store'
 
+db.defaults({
+  windowState: {
+    height: 563,
+    useContentSize: true,
+    width: 1000,
+    show: false,
+    minWidth: 300,
+    x: undefined,
+    y: undefined
+  },
+  appSettings: {
+    locale: 'zh-CN'
+  }
+}).write()
+
 export default {
 
 }
