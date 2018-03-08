d019fbf566023cfa8d50d3c498a4be1ab9b7a607
mtobeiyf <mtobeiyf@163.com>
2018-03-08 10:50:08 +0800

    :ambulance: Fix production data storage path for 0.2.0

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -2,9 +2,27 @@ const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
 const path = require('path')
+const fs = require('fs')
 
-const dataAdapter = new FileSync('db.json')
-const settingsAdapter = new FileSync('settings.json')
+const remote = require('electron').remote
+const app = require('electron').app
+
+const APP = process.type === 'renderer' ? remote.app : app
+const STORE_PATH = APP.getPath('userData')
+
+if (process.type !== 'renderer') {
+  if (!fs.existsSync(STORE_PATH)) {
+    fs.mkdirpSync(STORE_PATH)
+  }
+}
+
+const dataAdapter = (process.env.NODE_ENV !== 'development') ?
+  new FileSync(path.join(STORE_PATH, 'db.json')) :
+  new FileSync('db.json')
+const settingsAdapter = (process.env.NODE_ENV !== 'development') ?
+  new FileSync(path.join(STORE_PATH, 'settings.json')) :
+  new FileSync('settings.json')
+
 
 module.exports = {
   db: low(dataAdapter),
